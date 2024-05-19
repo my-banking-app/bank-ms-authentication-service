430ff451abfdb47f7c46ed1c029f25160e261bb9
camilobr89 <camilobr89@hotmail.com>
2024-05-19 16:51:02 -0500

    first commit

---
+++ src/main/java/com/bank/mybankingappauthenticationservice/MyBankingAppAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bank.mybankingappauthenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBankingAppAuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBankingAppAuthenticationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/mybankingappauthenticationservice/MyBankingAppAuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.mybankingappauthenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBankingAppAuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
