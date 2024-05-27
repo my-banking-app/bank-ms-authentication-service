3aea97706b4837ccb73f07aaf87fb6df0c7dfd00
camilobr89 <camilobr89@hotmail.com>
2024-05-27 18:26:49 -0500

    update CI/CD

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # bank-ms-authentication
 
 - Ejecucion en local con ambiente staging:  mvn spring-boot:run -Dspring-boot.run.profiles=staging
+- ingresar a la base de datos stg: docker exec -it auth-service-staging psql -U postgres -d staging_db
 
--- db_config/setup_users.sql
+++ db_config/setup_users.sql
@@ -13,6 +13,5 @@ INSERT INTO users (
     gender,
     data_processing_agreement
 ) VALUES
-('CC', '12345691', 'Alex', 'Jons', 30, 'Bogotá', 'Colombian', '3001234567', 'Single', 'alex.jons@example.com', crypt('Password3!', gen_salt('bf')), 'MALE', true),
-('CC', '12345692', 'Maria', 'Lopez', 28, 'Medellín', 'Colombian', '3001234568', 'Married', 'maria.lopez@example.com', crypt('Password4!', gen_salt('bf')), 'FEMALE', true),
-('CC', '12345693', 'Carlos', 'Smith', 35, 'Cali', 'Colombian', '3001234569', 'Divorced', 'carlos.smith@example.com', crypt('Password5!', gen_salt('bf')), 'MALE', true);
+('CC', '12345692', 'Maria', 'Lopez', 28, 'Medellín', 'Colombian', '3001234568', 'Married', 'maria.lopez@example.com', 'Password4!', 'FEMALE', true),
+('CC', '12345693', 'Carlos', 'Smith', 35, 'Cali', 'Colombian', '3001234569', 'Divorced', 'carlos.smith@example.com', ('Password5!', gen_salt('bf')), 'MALE', true);
