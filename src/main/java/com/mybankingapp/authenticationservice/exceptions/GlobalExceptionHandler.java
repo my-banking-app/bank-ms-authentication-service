353f9c54447c485c3ea1155904a7d70ab7bc32e5
camilobr89 <camilobr89@hotmail.com>
2025-05-30 14:11:31 -0500

    agrega restricción de unicidad para el número de identificación del usuario y valida su existencia al registrar

---
+++ src/main/java/com/mybankingapp/authenticationservice/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.mybankingapp.authenticationservice.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.CONFLICT.value());
+        body.put("error", "Datos inválidos");
+        body.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.CONFLICT);
+    }
+
+    // Puedes agregar más excepciones aquí si necesitas
+}
+
